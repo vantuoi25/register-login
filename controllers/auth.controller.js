090eb8896c84170ff7d3dd353a134eb77d611f26
vantuoi25 <K215246@tohokugaigo.com>
2023-09-16 03:51:31 +0900

    register login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,42 +7,44 @@ const ErrorResponse = require('../helpers/ErrorResponse');
 dotenv.config();
 
 module.exports = {
-    register: async (req, res) =>{
-            const { username, password } = req.body;
-            const { error, value} = accountValid(body);
+  register: async (req, res) => {
+    const body = req.body;
+    const { error, value } = accountValid(body);
 
-            if(error){
-                throw new ErrorResponse(400, 'TK or MK fail!!');
-            }
-            const newAccount = await accountModel.create(body);
-            return res.status(201).json(newAccount);
-            },
-        
-    login: async (req, res) =>{
-       const {username, password} = req.body;
-       const account = await accountModel.findOne({
-            username: username,
-       });
-       if(!account){
-        throw new ErrorResponse(401, 'TK or MK fail!');
-       }
-
-       const checkPass = bcryptjs.compareSync(password, account.password);
-       if(!checkPass){
-        throw new ErrorResponse(401, 'TK or MK fail!');
-       }
-
-       const payload = {
-        _id: account._id,
-        username: account.username,
-       };
-
-       const token = jwt.sign(payload, process.env.SECRET_KEY);
-
-       return res.status(200).json({
-        _id: account._id,
-        username:account.username,
-        jwt: token,
-       });
+    if (error) {
+      throw new ErrorResponse(400, 'TK or MK fail!!');
     }
+    const newAccount = await accountModel.create(body);
+    return res.status(201).json(newAccount);
+  },
+
+  login: async (req, res) => {
+    const { username, password } = req.body;
+    const account = await accountModel.findOne({
+      username: username,
+    });
+    if (!account) {
+      throw new ErrorResponse(401, 'TK or MK fail!');
+    }
+
+    const checkPass = bcryptjs.compareSync(password, account.password);
+    if (!checkPass) {
+      throw new ErrorResponse(401, 'TK or MK fail!');
+    }
+
+    const payload = {
+      _id: account._id,
+      username: account.username,
+    };
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: '1h'
+    });
+
+    return res.status(200).json({
+      _id: account._id,
+      username: account.username,
+      jwt: token,
+    });
+  },
 };
